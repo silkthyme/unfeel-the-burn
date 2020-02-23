781b0e12606137842ec020a5f14cec2c92e68207
Asim Biswal <abiswal@berkeley.edu>
2020-02-22 23:21:47 -0800

    added model to backend

--- service/app.py
+++ service/app.py
@@ -6,6 +6,11 @@ import numpy as np
 import os
 from skimage.transform import resize
 from skimage.io import imread
+import pickle
+import pandas as pd
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+
 
 static_path = os.path.join('client')
 
@@ -42,3 +47,13 @@ def transform():
     output.headers["Content-Disposition"] = "attachment; filename=image.csv"
     output.headers["Content-type"] = "text/csv"
     return output
+
+
+def classify(csv) :
+    f = open('model2.py', 'rb')
+    classifier = pickle.load(f)
+    table = pd.read_csv(csv)
+    X = table.values
+    prediction = classifier.predict(X)
+    print(prediction[0])
+    return prediction[0]
