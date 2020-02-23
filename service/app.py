d36c6a8092c95a19d85183378a042b52c495240e
christina <christinahuangji@gmail.com>
2020-02-23 00:12:57 -0800

    minor merge resolve

--- service/app.py
+++ service/app.py
@@ -10,11 +10,7 @@ import pickle
 import pandas as pd
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
-<<<<<<< HEAD
-
-=======
 from PIL import Image
->>>>>>> csvdownloading
 
 static_path = os.path.join('client')
 
